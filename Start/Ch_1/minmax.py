627fbed4696aadced1e37a4dbb6a2c02866b6c82
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2024-08-06 12:21:41 +0400

    min-max

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -24,4 +24,15 @@ strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 # TODO: open the data file and load the JSON
 with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
-print(data)
+    
+print(data["metadata"]["title"])
+print(len(data["features"]))
+
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+print(min(data["features"], key=getmag))
+print(max(data["features"], key=getmag))
